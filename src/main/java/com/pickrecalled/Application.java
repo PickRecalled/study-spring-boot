258208d836a4d764906580c7f80f2953cfb7548f
pickrecalled <pickrecalled@163.com>
2018-08-01 16:21:23 +0800

    @ImportResource实现例子

--- src/main/java/com/pickrecalled/Application.java
+++ src/main/java/com/pickrecalled/Application.java
@@ -2,12 +2,13 @@ package com.pickrecalled;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * @SpringBootApplication 来标注一个主程序，说明这是一个spring boot应用
  */
 @SpringBootApplication
-//@ImportResource(locations = {"classpath:spring-service.xml"})
+@ImportResource(locations = {"classpath:spring-service.xml"})
 public class Application {
     public static void main(String[] args) {
         //spring运行主程序
